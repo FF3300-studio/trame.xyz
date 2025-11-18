6b9d0e30f07fe686ffb63de5e410039f86c73df2
Alberto Guerra <a.alb.guerra@gmail.com>
2025-11-18 16:13:37 +0100

    style cards

--- site/snippets/card-info.php
+++ site/snippets/card-info.php
@@ -20,6 +20,40 @@
 </div>
 <?php endif; ?>
 
+    <div class="cards-thumbnail">
+      <?php if ($thumb_toggle && $item->thumbnail()->isNotEmpty()): ?>
+        <?php
+          $image = $item->thumbnail()->toFile();
+          $ratio = $image->width() / $image->height();
+        ?>
+        <div class="card-image-container">
+          <img
+            class="lazyload"
+            data-sizes="auto"
+            data-src="<?= $image->resize(1280)
+                            ->thumb(['format' => 'webp', 'quality' => 75])
+                            ->url() ?>"
+            data-srcset="<?= $image->srcset(
+                              [320, 640, 960, 1280, 1600, 1920],
+                              ['format' => 'webp', 'quality' => 75]
+                            ) ?>"
+            width="<?= $image->width() ?>"
+            height="<?= $image->height() ?>"
+            alt="<?= html($image->alt() ?? $image->filename()) ?>"
+            style="
+              <?php if (isset($min_height) && $min_height !== ''): ?>min-height: <?= $min_height ?>;<?php endif ?>
+              <?php if (isset($min_width)  && $min_width  !== ''): ?>min-width: <?= $min_width  ?>;<?php endif ?>
+              <?php if (isset($max_height) && $max_height !== ''): ?>max-height: <?= $max_height ?>;<?php endif ?>
+              <?php if (isset($max_width)  && $max_width  !== ''): ?>max-width: <?= $max_width  ?>;<?php endif ?>
+              aspect-ratio: <?= $ratio ?>;
+              object-fit: <?= ($contain ?? false) ? 'contain' : 'cover' ?>;
+              width: 100%;
+            "
+          >
+        </div>
+      <?php endif ?>
+    </div>
+
 <div class="cards-title" style="margin:0!important;">
     <h2><?= $item->title() ?></h2>
 </div>
