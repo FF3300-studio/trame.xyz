625c3617cce8efe366fe3098ecc3e6f82375b6a8
Alberto Guerra <a.alb.guerra@gmail.com>
2025-12-17 12:33:55 +0100

    footer istituzionale

--- site/snippets/blocks/image.php
+++ site/snippets/blocks/image.php
@@ -8,6 +8,9 @@ $link    = $block->link();
 $ratio   = $block->ratio()->or('auto');
 $src     = null;
 $max_width = $block->max_width();
+$max_height = $block->max_height();
+$min_width = $block->min_width();
+$min_height = $block->min_height();
 
 if ($block->location() == 'web') {
     $src = $block->src()->esc();
@@ -23,13 +26,25 @@ if ($block->location() == 'web') {
         <a href="<?= $link->url() ?>" title="<?= $block->id() ?>">
         <?php if($image->extension() == 'gif'): ?>
             <img    class="lazyload"
-                    style="aspect-ratio: <?= $ratio ?>; object-fit: cover; max-width: <?= $max_width ?>px;"
+                    style="
+                    <?php if($ratio !== NULL OR $ratio !== ''): ?>aspect-ratio: <?= $ratio ?>;<?php else: ?><?php endif; ?> 
+                    <?php if($max_width !== NULL OR $max_width !== ''): ?>object-fit: cover; max-width: <?= $max_width ?>; <?php else: ?>max-width: fit-content;<?php endif; ?> 
+                    <?php if($min_width !== NULL OR $min_width !== ''): ?>min-width: <?= $min_width ?>;<?php else: ?>min-width: fit-content;<?php endif; ?> 
+                    <?php if($max_height !== NULL OR $max_height !== ''): ?>max-height: <?= $max_height ?>;<?php else: ?>max-height: fit-content;<?php endif; ?>
+                    <?php if($min_height !== NULL OR $min_height !== ''): ?>min-height: <?= $min_height ?>;<?php else: ?>min-height: fit-content;<?php endif; ?> 
+                    "
                     src="<?= $src ?>"
                     data-src="<?= $src ?>" 
                     alt="<?= $block->alt()->or($image->alt()) ?>">
         <?php else: ?>
             <img    class="lazyload"
-                    style="aspect-ratio: <?= $ratio ?>; object-fit: cover; max-width: <?= $max_width ?>px;"
+                    style="
+                    <?php if($ratio !== NULL OR $ratio !== ''): ?>aspect-ratio: <?= $ratio ?>;<?php else: ?><?php endif; ?> 
+                    <?php if($max_width !== NULL OR $max_width !== ''): ?>object-fit: cover; max-width: <?= $max_width ?>; <?php else: ?>max-width: fit-content;<?php endif; ?> 
+                    <?php if($min_width !== NULL OR $min_width !== ''): ?>min-width: <?= $min_width ?>;<?php else: ?>min-width: fit-content;<?php endif; ?> 
+                    <?php if($max_height !== NULL OR $max_height !== ''): ?>max-height: <?= $max_height ?>;<?php else: ?>max-height: fit-content;<?php endif; ?>
+                    <?php if($min_height !== NULL OR $min_height !== ''): ?>min-height: <?= $min_height ?>;<?php else: ?>min-height: fit-content;<?php endif; ?> 
+                    "
                     data-src="<?= $image->thumb()->url() ?>" 
                     alt="<?= $block->alt()->or($image->alt()) ?>">
         <?php endif; ?>
@@ -39,13 +54,25 @@ if ($block->location() == 'web') {
             <img    class="lazyload"
                     src="<?= $src ?>"
                     data-src="<?= $src ?>" 
-                    style="aspect-ratio: <?= $ratio ?>; object-fit: cover; max-width: <?= $max_width ?>px;"
+                    style="
+                    <?php if($ratio !== NULL OR $ratio !== ''): ?>aspect-ratio: <?= $ratio ?>;<?php else: ?><?php endif; ?> 
+                    <?php if($max_width !== NULL OR $max_width !== ''): ?>object-fit: cover; max-width: <?= $max_width ?>; <?php else: ?>max-width: fit-content;<?php endif; ?> 
+                    <?php if($min_width !== NULL OR $min_width !== ''): ?>min-width: <?= $min_width ?>;<?php else: ?>min-width: fit-content;<?php endif; ?> 
+                    <?php if($max_height !== NULL OR $max_height !== ''): ?>max-height: <?= $max_height ?>;<?php else: ?>max-height: fit-content;<?php endif; ?>
+                    <?php if($min_height !== NULL OR $min_height !== ''): ?>min-height: <?= $min_height ?>;<?php else: ?>min-height: fit-content;<?php endif; ?> 
+                    "
                     alt="<?= $block->alt()->or($image->alt()) ?>">
         <?php else: ?>
             <img    class="lazyload"
                     src="<?= $src ?>"
                     data-src="<?= $src ?>" 
-                    style="aspect-ratio: <?= $ratio ?>; object-fit: cover; max-width: <?= $max_width ?>px;"
+                    style="
+                    <?php if($ratio !== NULL OR $ratio !== ''): ?>aspect-ratio: <?= $ratio ?>;<?php else: ?><?php endif; ?> 
+                    <?php if($max_width !== NULL OR $max_width !== ''): ?>object-fit: cover; max-width: <?= $max_width ?>; <?php else: ?>max-width: fit-content;<?php endif; ?> 
+                    <?php if($min_width !== NULL OR $min_width !== ''): ?>min-width: <?= $min_width ?>;<?php else: ?>min-width: fit-content;<?php endif; ?> 
+                    <?php if($max_height !== NULL OR $max_height !== ''): ?>max-height: <?= $max_height ?>;<?php else: ?>max-height: fit-content;<?php endif; ?>
+                    <?php if($min_height !== NULL OR $min_height !== ''): ?>min-height: <?= $min_height ?>;<?php else: ?>min-height: fit-content;<?php endif; ?> 
+                    "
                     alt="<?= $block->alt()->or($image->alt()) ?>">
         <?php endif; ?>
     <?php endif ?>
