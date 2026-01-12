f0cb8f18122d6f686c8c8eeb8f1ee72d4366476c
Alberto Guerra <a.alb.guerra@gmail.com>
2026-01-12 12:32:22 +0100

    inizio setup lingue anche da backend

--- site/languages/es.php
+++ site/languages/es.php
@@ -12,7 +12,7 @@ return [
     'translations' => [
         
     ],
-    'url' => '/'
+    'url' => NULL
 ];
 
 ?>
--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -5,6 +5,13 @@
       <?php snippet('logo-object',[]); ?>
       <div class="navigation navigation-desktop">
         <?php snippet('menuitem-list', ['items' => $items, 'accordion__item' => true]) ?>
+        <?php if($site->languages_switch()->toBool()): ?>
+          <?php foreach($kirby->languages() as $language): ?>
+            <a class="title_link <?php e($kirby->language() == $language, 'active') ?>" href="<?= $page->url($language->code()) ?>" hreflang="<?php echo $language->code() ?>" style="font-family: 'Grid'; font-variation-settings: 'wght' 1000;">
+              <?= html($language->code()) ?>
+            </a>
+          <?php endforeach ?>
+        <?php endif ?>
       </div>
       <div class="navbar-toggler closed">
 			  <span class="icon-bar"></span>
@@ -15,6 +22,13 @@
     <div class="navigation navigation-mobile">
       <div class="navigation-mobile-flexbox">
         <?php snippet('mobile-menuitem-list', ['items' => $items, 'accordion__item' => true]) ?>
+        <?php if($site->languages_switch()->toBool()): ?>
+          <?php foreach($kirby->languages() as $language): ?>
+            <a class="title_link <?php e($kirby->language() == $language, 'active') ?>" href="<?= $page->url($language->code()) ?>" hreflang="<?php echo $language->code() ?>" style="font-family: 'Grid'; font-variation-settings: 'wght' 1000;">
+              <?= html($language->code()) ?>
+            </a>
+          <?php endforeach ?>
+        <?php endif ?>
       </div>
     </div>
     <?php endif ?>
