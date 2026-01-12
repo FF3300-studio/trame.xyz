a93beda8c5e970edbe45004c7b7c4017bbcf59a3
Alberto Guerra <a.alb.guerra@gmail.com>
2026-01-12 12:13:33 +0100

    tag colori

--- site/snippets/color_cat_chips.php
+++ site/snippets/color_cat_chips.php
@@ -1,6 +1,9 @@
         <?php foreach($tutte_le_categorie as $cat_item): ?>
             <?php if($cat_item->nome() == $category): ?>
                 background-color: <?= $cat_item->colore_categoria() ?>;
+                <?php if($cat_item->colore_testo()->isNotEmpty()): ?>
+                    color: <?= $cat_item->colore_testo() ?> !important;
+                <?php endif; ?>
             <?php endif; ?>
         <?php endforeach; ?>
         
